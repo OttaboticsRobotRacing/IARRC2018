cba0a7a19b4dd3eee1d91dcce17a11e6a3ed035b
jimmy <livingoscilloscope@gmail.com>
2018-05-18 22:39:26 -0400

    added script to modify arduino file permissions

---
+++ communication/set_arduino_permissions.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo chown iarrc:iarrc arduino_master/arduino_master.ino
+sudo chmod 777 arduino_master/arduino_master.ino
+
+sudo chown iarrc:iarrc arduino_slave_0/arduino_slave_0.ino
+sudo chmod 777 arduino_slave_0/arduino_slave_0.ino
