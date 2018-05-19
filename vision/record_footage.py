3b5dbbd73e322d6a31071d81b0ff5ee17995cc38
jimmy <livingoscilloscope@gmail.com>
2018-05-19 15:35:51 -0400

    modified to not display the frame

--- vision/record_footage.py
+++ vision/record_footage.py
@@ -21,7 +21,7 @@ while(cap.isOpened()):
         # write the flipped frame
         out.write(frame)
 
-        cv2.imshow('frame',frame)
+        #cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
