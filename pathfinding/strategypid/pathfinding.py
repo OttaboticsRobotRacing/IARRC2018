c44eed4cac6c6f0eb50a98cde7023a2947ee7085
jimmy <livingoscilloscope@gmail.com>
2018-05-19 20:28:14 -0400

    added clustering pathfinder code

---
+++ pathfinding/strategypid/pathfinding.py
@@ -0,0 +1,303 @@
+import constants
+import cv2
+import numpy as np
+import math
+import utility
+import logging
+
+
+def compute_turn_angle(image):
+    # segment the frame horizontally
+    subframe_list = split_frame(image, constants.NUMBER_OF_SUBDIVISIONS)
+
+    # get line segments
+    # 2D array - list of lists of line segments
+    list_of_segment_lists = []
+    for subframe in subframe_list:
+        list_of_segment_lists.append(get_line_segments(subframe))
+
+    # cluster lines by angle
+    clusters_list = []
+    for segment_list in list_of_segment_lists:
+        if len(segment_list) != 0:
+            clusters_list.append(cluster_by_angle(segment_list))  # cluster by angle takes a list of Line objects
+
+    # find longest line cluster
+    raw_output_list = []
+    for clusters in clusters_list:
+        # TODO: need to somehow find the longest cluster in a list of clusters
+        # each cluster is a list of line segments
+        # cluster_list is a 3D array
+
+        largest_cluster_length = 0
+        largest_cluster = None
+        for cluster in clusters:
+            cluster_magnitude = compute_cluster_magnitude(cluster)
+            if cluster_magnitude > largest_cluster_length:
+                largest_cluster_length = cluster_magnitude
+                largest_cluster = cluster
+
+        raw_output_list.append(compute_cluster_direction(largest_cluster))
+
+    # logging message
+    logging.debug('pathfinder.compute_turn_angle()')
+    logging.debug('raw_output_list: %s' % raw_output_list)
+
+    # generate weights for each subframe
+    #num_nonzero_segment_lists = sum(len(x) > 0 for x in list_of_segment_lists)
+    weight_list = generate_weight_list(constants.NUMBER_OF_SUBDIVISIONS)
+    #weight_list = generate_weight_list(num_nonzero_segment_lists)
+
+
+    # apply weighted sum to dominant line angles
+    weighted_output_angle_list = []
+    for i, output_angle in enumerate(raw_output_list):
+        weighted_output = weight_list[i] * output_angle
+        weighted_output_angle_list.append(weighted_output)
+
+    # set final output angle to the sum of angles (with weights applied)
+    final_output_angle = 0
+    for output_angle in weighted_output_angle_list:
+        final_output_angle += output_angle
+
+    return final_output_angle
+
+
+# Helper classes and functions
+
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        # set start and end points
+        self.p1 = (x1, y1)
+        self.p2 = (x2, y2)
+
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+        # set length
+        self.length = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+        # compute line angle
+        dx = x2 - x1
+        dy = y2 - y1
+        rads = math.atan2(-dy, dx)
+        rads %= 2 * math.pi
+        degs = -math.degrees(rads)
+        if degs <= -180:
+            degs += 180
+        degs += 90
+        self.angle = degs
+
+        # set midpoint
+        self.midpoint = (((x1 + x2) / 2), ((y1 + y2) / 2))
+
+    def __str__(self):
+        return str(self.angle)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get_coords(self):
+        return self.p1, self.p2
+
+
+class Cluster:
+    def __init__(self, cluster):
+        self.cluster = cluster
+        self.magnitude = compute_cluster_magnitude(cluster)
+        self.position = compute_cluster_position(cluster)
+
+
+def get_line_segments(image):
+    """
+    Find the line segments in the frame using HoughLinesP
+    :return:
+    """
+    mask_edges = cv2.Canny(image, 0, 200, apertureSize=3)
+
+    # variables for HoughLinesP
+
+    # max_line_gap = 20 # default/previous/initial value
+    max_line_gap = 5
+    min_line_length = 50
+    threshold = 20
+
+    lines = cv2.HoughLinesP(mask_edges, 1, np.pi/180, threshold, min_line_length, max_line_gap)
+
+    # create list of Line objects
+    line_object_list = []
+
+    try:
+        for line in lines:
+            # print repr(line)
+            x1 = line[0][0]
+            y1 = line[0][1]
+            x2 = line[0][2]
+            y2 = line[0][3]
+            line_object_list.append(Line(x1, y1, x2, y2))
+    except:
+        pass
+
+    return line_object_list
+
+
+def cluster_by_angle(line_list):
+    clusters = []
+    last_angle = -180
+
+    for line in line_list:
+        if abs(last_angle - line.angle) > 5:
+            clusters.append([line])
+        else:
+            clusters[-1].append(line)
+        last_angle = line.angle
+    return clusters
+
+
+def cluster_by_proximity(image, line_list):
+    clusters = []
+
+    for line in line_list:
+        clusters.append([line])
+        for query_line in line_list:
+            distance = compute_point_distance(line.midpoint, query_line.midpoint)
+            if distance <= 100 and abs(line.angle - query_line.angle) < 10:
+                clusters[-1].append(query_line)
+                cv2.line(image, line.midpoint, query_line.midpoint, (128, 128, 128), 1)
+
+    filtered_clusters = []
+    for cluster in clusters:
+        if len(cluster) >= constants.CLUSTER_MIN_SIZE:
+            filtered_clusters.append(cluster)
+
+    return image, filtered_clusters
+
+
+def compute_point_distance(p1, p2):
+    x1 = p1[0]
+    y1 = p1[1]
+    x2 = p2[0]
+    y2 = p2[1]
+
+    return abs(math.sqrt((x1 - x2)**2 + (y1 - y2)**2))
+
+
+def compute_cluster_position(cluster):
+    """
+
+    :param cluster:
+    :return:
+    """
+    x_total = 0
+    y_total = 0
+
+    for line in cluster:
+        x_total += line.midpoint[0]
+        y_total += line.midpoint[1]
+
+    return x_total / len(cluster), y_total / len(cluster)
+
+
+def compute_cluster_magnitude(cluster):
+    """
+
+    :param cluster:
+    :return:
+    """
+    length = 0
+
+    for line in cluster:
+        length += line.length
+
+    return length
+
+
+def compute_cluster_direction(cluster):
+    angle_sum = 0
+    for line_segment in cluster:
+        angle_sum += line_segment.angle
+
+    return angle_sum / len(cluster)
+
+
+def generate_weight_list(number_of_weights):
+    weight_list = []
+    for i in range(number_of_weights):
+        weight_list.append(1.0 / 2**(i+1))
+
+    weight_sum = 0
+    for w in weight_list:
+        weight_sum += w
+
+    weight_diff = 1 - weight_sum
+    weight_comp = weight_diff / number_of_weights
+
+    for i, w in enumerate(weight_list):
+        weight_list[i] += weight_comp
+
+    weight_sum = 0
+    for w in weight_list:
+        weight_sum += w
+
+    # logging message
+    logging.debug('pathfinder.generate_weight_list()')
+    logging.debug('weight_list: %s' % weight_list)
+    logging.debug('weight_sum: %s' % weight_sum)
+
+    return weight_list
+
+
+def split_frame(image, number_of_subimages):
+    """
+    Split an image into a list of subimages
+    :param image:
+    :param number_of_subimages:
+    :return:
+    """
+    initial_x = image.shape[1]
+    initial_y = image.shape[0]
+
+    final_y = int(initial_y / number_of_subimages)
+
+    subimage_list = []
+
+    start_index = 0
+    end_index = final_y
+
+    for i in range(number_of_subimages):
+        try:
+            subimage_list.append(image[:][start_index:end_index])
+            start_index = end_index + 1
+            end_index += final_y
+        except Exception as e:
+            utility.get_stacktrace()
+
+    return subimage_list
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if ret:
+            frame = utility.resize_image(frame, 480)
+            print('Output turn angle: %s' % compute_turn_angle(frame))
+            cv2.imshow('frame', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
---
+++ pathfinding/strategypid/utility.py
@@ -0,0 +1,279 @@
+import cv2
+import constants
+import numpy as np
+
+
+def apply_preprocessing(image):
+    """
+    Apply suite of basic image processing filters like blur and colour conversion
+    :param image:
+    :return:
+    """
+    # convert colour space from BGR to grey to BGR
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    # resize the image to height 480
+    image = resize_image(image, constants.IMAGE_HEIGHT)
+    # apply gaussian blur
+    image = cv2.GaussianBlur(image, (3, 3), 0)
+
+    return image
+
+
+def write_to_log(message, log_file="LOG.txt"):
+    with open(log_file, "a") as f:
+        f.write(message)
+
+
+def save_image(image, file_name):
+    cv2.imwrite("SavedImages/" + file_name, image)
+
+
+def record_video():
+    pass
+
+
+def apply_perspective_transformation_old(image):
+    height = image.shape[0]
+    width = image.shape[1]
+
+    final_top_left = [60, 155]
+    final_top_right = [155, 60]
+    final_bottom_left = [325, 420]
+    final_bottom_right = [420, 325]
+
+    if constants.DEBUG_MESSAGES:
+        print("DEBUG:: Utility.apply_perspective_transformation()")
+        print("DEBUG:: height = %s" % height)
+        print("DEBUG:: width = %s" % width)
+
+        pts = np.array([tuple(final_top_left),
+                        tuple(final_top_right),
+                        tuple(final_bottom_right),
+                        tuple(final_bottom_left)], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(image, [pts], True, (0, 255, 255))
+
+        cv2.putText(image, "TL", tuple(final_top_left), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0))
+        cv2.putText(image, "TR", tuple(final_top_right), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0))
+        cv2.putText(image, "BL", tuple(final_bottom_left), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0))
+        cv2.putText(image, "BR", tuple(final_bottom_right), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0))
+
+        cv2.imshow("DEBUG-apply_perspective_transformation", image)
+
+    # [top-left], [top-right], [bottom-left], [bottom-right]
+    final_pts = np.float32([[0, 0],
+                            [width - 1, 0],
+                            [0, height - 1],
+                            [width - 1, height - 1]])
+    image_pts = np.float32([final_top_left,
+                            final_top_right,
+                            final_bottom_left,
+                            final_bottom_right])
+
+    try:
+        M = cv2.perspectiveTransform(image_pts, final_pts)
+
+        # max width, max height
+        output = cv2.warpPerspective(image, M, (width, height))
+
+        return output
+    except:
+        get_stacktrace()
+        return image
+
+
+def four_point_transform(pts=None):
+    # POC
+    final_top_left = (60, 155)
+    final_top_right = (155, 60)
+    final_bottom_left = (325, 420)
+    final_bottom_right = (420, 325)
+
+    pts = np.array([final_top_left,
+                    final_top_right,
+                    final_bottom_right,
+                    final_bottom_left],
+                   dtype="float32")
+
+    max_width, max_height = 450, 450
+    hwratio = 11 / 8.5  # letter size paper
+    scale = int(max_width / 12)
+
+    # center_x = 150
+    center_x = int(max_width / 2)
+    # center_y = 250
+    center_y = int(max_height * 2/3)
+
+    dst = np.array([
+        [center_x - scale, center_y - scale * hwratio],  # top left
+        [center_x + scale, center_y - scale * hwratio],  # top right
+        [center_x + scale, center_y + scale * hwratio],  # bottom right
+        [center_x - scale, center_y + scale * hwratio],  # bottom left
+    ], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(pts, dst)
+
+    return M
+
+
+def apply_perspective_transformation(image):
+    final_top_left = (228, 170)
+    final_top_right = (500, 210)
+    final_bottom_left = (225, 325)
+    final_bottom_right = (515, 325)
+
+    pts = np.array([final_top_left,
+                    final_top_right,
+                    final_bottom_right,
+                    final_bottom_left],
+                   dtype="float32")
+
+    max_height = image.shape[0]
+    max_width = image.shape[1]
+    hwratio = constants.PAPER_DIMENSIION_HEIGHT / constants.PAPER_DIMENSION_WIDTH  # letter size paper
+    scale = int(max_width / constants.PERSPECTIVE_TRANSFORM_SCALE_FACTOR)
+
+    center_x = int(max_width / 2)
+    center_y = int(max_height * 2 / 3)
+
+    dst = np.array([
+        [center_x - scale, center_y - scale * hwratio],  # top left
+        [center_x + scale, center_y - scale * hwratio],  # top right
+        [center_x + scale, center_y + scale * hwratio],  # bottom right
+        [center_x - scale, center_y + scale * hwratio],  # bottom left
+    ], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(pts, dst)
+
+    try:
+        output = cv2.warpPerspective(image, M, (max_width, max_height))
+
+        return output
+    except:
+        get_stacktrace()
+        return image
+
+
+def generate_perspective_transformation_params():
+    pass
+
+
+def resize_image(img, height):
+    """
+    Resizes an image to the specified height
+    :param img:
+    :param height:
+    :return:
+    """
+    ratio = float(height) / img.shape[0]
+    dim = (int(img.shape[1]*ratio), height)
+
+    image = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    return image
+
+
+def split_frame(image, number_of_subimages):
+    """
+    Split an image into a list of subimages
+    :param image:
+    :param number_of_subimages:
+    :return:
+    """
+    initial_x = image.shape[1]
+    initial_y = image.shape[0]
+
+    final_y = int(initial_y / number_of_subimages)
+
+    subimage_list = []
+
+    start_index = 0
+    end_index = final_y
+
+    for i in range(number_of_subimages):
+        try:
+            subimage_list.append(image[:][start_index:end_index])
+            start_index = end_index + 1
+            end_index += final_y
+        except Exception as e:
+            get_stacktrace()
+
+    return subimage_list
+
+
+def get_roi(img, points):
+    """
+    Returns image after being cropped to coordinates defined by the list points
+    :param img:
+    :param points:
+    :return:
+    """
+    x_list = []
+    y_list = []
+    for i in range(0, len(points), 2):
+        x_list.append(points[i])
+    for i in range(1, len(points), 2):
+        y_list.append(points[i])
+
+    tl = (min(x_list), min(y_list))
+    br = (max(x_list), max(y_list))
+
+    roi = img[tl[1]:br[1], tl[0]:br[0]]
+
+    # print("top left coords: " + str(tl))
+    # print("bottom right coords: " + str(br))
+
+    return roi
+
+
+def get_colour_bgr(img, point):
+    """
+    Returns the colour in BGR format for an image at a specified point (x,y)
+    """
+    colour = img[point[0]][point[1]].ravel().tolist()
+    return colour
+
+
+def get_colour_hsv(img, point):
+    """
+    Returns the colour in BGR format for an image at a specified point (x,y)
+    """
+    try:
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    except Exception as e:
+        get_stacktrace()
+        img_hsv = img
+    colour = img[point[0]][point[1]].ravel().tolist()
+    colour[0] = int(colour[0] * 2)
+    colour[1] = int(colour[1] / 255.0 * 100.0)
+    colour[2] = int(colour[2] / 255.0 * 100.0)
+    return colour
+
+
+def get_stacktrace():
+    import traceback
+    print("======================")
+    print("<< Exception caught >>")
+    print("======================")
+    traceback.print_exc()
+    print("======================")
+    error = traceback.format_exc()
+    write_to_log(error)
+    return error
+
+
+class PIDController:
+    def __init__(self, start_value=0):
+        self.error_integral = 0
+        self.k_p = 1
+        self.k_i = 1
+        self.k_d = 0
+        self.value = start_value
+
+    def compute_error(self, desired_value):
+        return desired_value - self.value
+
+    def compute_next_value(self, desired_value):
+        pass
